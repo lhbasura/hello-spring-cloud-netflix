d8276ee090d93ec360b9e6007d8973db90f9fe05
lhbasura <lhbasura@gmail.com>
2019-09-28 21:01:07 +0800

    init

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/lhbasura/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.lhbasura.hello.spring.cloud.web.admin.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminRibbonApplication {
+    public static void main(String[]args)
+    {
+        SpringApplication.run(WebAdminRibbonApplication.class,args);
+    }
+}
